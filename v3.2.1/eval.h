242341b115b559126dff01962bbded4fc1610856
Philippe Choquette <phil_de_choc@hotmail.com>
2016-01-20 22:09:53 -0500

    release version

---
+++ v3.2.1/eval.h
@@ -0,0 +1,27 @@
+/*
+ * by Parent and Childset Accessible Syntax Tree Language Interpreter
+ * Copyright (C) 2008-2016  Philippe Choquette
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef EVAL_H
+#define EVAL_H
+
+#include "node.h"
+
+data eval_func_call(node * to_eval);
+data eval(node * to_eval);
+
+#endif
